f9327418db384b96d90ad3ceb9e6ce2d04aa4c62
A.Dante <nuzzi.a86@hotmail.com>
2018-02-03 21:23:40 +0100

    From Eclipse

--- src/main.java
+++ src/main.java
@@ -2,9 +2,9 @@
 public class main {
 
 	public static void main(String[] args) {
-	
-	System.out.println("bubibubibubi");	
-
+		
+		System.out.println("bubibubibubi");	
+		System.out.println("bubibubibubi");	
 	}
 
 }
