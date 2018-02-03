b3f238a4c5465e1adaca93aa75df7e5a8113a1a7
A.Dante <nuzzi.a86@hotmail.com>
2018-02-03 21:51:39 +0100

    added

--- src/main.java
+++ src/main.java
@@ -3,9 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("bubibubibubi");	
-		System.out.println("bubibubibubi");	
-		System.out.println("hello from RicDev");
+		System.out.println("hello");
 	}
 
 }
