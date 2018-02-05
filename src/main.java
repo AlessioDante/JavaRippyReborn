a04676055913dbf136bfe4e0638f016be4512eeb
Riccardo <riccardo.toni.85@gmail.com>
2018-02-05 13:23:00 +0100

    committy

--- src/main.java
+++ src/main.java
@@ -1,43 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
 
 public class main {
 
-	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	String[] sloganList = {"Ps Mangio biscotti gtfo","grrr puzzi", "1,2,3 cacca in culo a te", "Polep Fresser", "Dont mess with cnabo or get the stabbo", " hello there"};	
-	
-	public class Bp_Cnaby {
-		Bp_Cnaby saySomething(){
-			Random rand = new Random();
-			System.out.println("cnaby dice : " + sloganList[rand.nextInt(5)]);
-			return this;
-		}
-	}
-	
-	public void ale_hello() {
-		System.out.println("im ale");
-	}
-
-	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	//
-	// Rik SandBox 
-	//
-	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	
-	public void rik_hello() {
-		System.out.println("im rik");
-	}
-	
-	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-	
 	public static void main(String[] args) {
-		
-		Bp_Cnaby CnabyRik = new main().new Bp_Cnaby();
-		CnabyRik.saySomething().saySomething().saySomething().saySomething().saySomething();
+
+		System.out.println("pls");
+
 	}
 }
