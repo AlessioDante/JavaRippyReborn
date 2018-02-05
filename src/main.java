0f90c448d6dac0d3b1f4c3b8a8b812848a15f168
Riccardo <riccardo.toni.85@gmail.com>
2018-02-05 13:56:05 +0100

    undo

--- src/main.java
+++ src/main.java
@@ -1,9 +1,40 @@
 
+import java.util.Random;
+
 public class main {
 
+	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	String[] sloganList = {"Ps Mangio biscotti gtfo","grrr puzzi", "1,2,3 cacca in culo a te", "Polep Fresser", "Dont mess with cnabo or get the stabbo", " hello there"};	
+	
+	public class Bp_Cnaby {
+		Bp_Cnaby saySomething(){
+			Random rand = new Random();
+			System.out.println("cnaby dice : " + sloganList[rand.nextInt(5)]);
+			return this;
+		}
+	}
+	
+	public void ale_hello() {
+		System.out.println("im ale");
+	}
+
+	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	//
+	// Rik SandBox 
+	//
+	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	
+	public void rik_hello() {
+		System.out.println("im rik");
+		System.out.println("switch from older commit");
+	}
+	
+	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+	
 	public static void main(String[] args) {
-
-		System.out.println("pls");
-
+		
+		Bp_Cnaby CnabyRik = new main().new Bp_Cnaby();
+		CnabyRik.saySomething().saySomething().saySomething().saySomething().saySomething();
 	}
 }
