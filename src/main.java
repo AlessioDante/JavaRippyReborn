56a7e448d9aa79aacbde3bdcfbf186ed1b1dca7d
A.Dante <nuzzi.a86@hotmail.com>
2018-02-09 23:32:33 +0100

    Something new

--- src/main.java
+++ src/main.java
@@ -3,14 +3,27 @@ import java.util.Random;
 
 public class main {
 
-	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	///+-+-+-+-+-+-+-+-+-+-+-+-
+	// Ale SandBox 
+	///+-+-+-+-+-+-+-+-+-+-+-+-
+	
+	//+-+-+- Variabili -+-+-+//
 	String[] sloganList = {"Ps Mangio biscotti gtfo","grrr puzzi", "1,2,3 cacca in culo a te", "Polep Fresser", "Dont mess with cnabo or get the stabbo", " hello there"};	
 	
 	public class Bp_Cnaby {
+		private String nome;
+		
 		Bp_Cnaby saySomething(){
 			Random rand = new Random();
 			System.out.println("cnaby dice : " + sloganList[rand.nextInt(5)]);
 			return this;
+		}
+		
+		public void setNome(String nnome) {
+			nome = nnome;
+		}
+		public String getNome() {
+			return nome;
 		}
 	}
 	
@@ -18,23 +31,22 @@ public class main {
 		System.out.println("im ale");
 	}
 
-	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	//
+	///+-+-+-+-+-+-+-+-+-+-+-+-
 	// Rik SandBox 
-	//
-	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	
+	///+-+-+-+-+-+-+-+-+-+-+-+-
 	public void rik_hello() {
 		System.out.println("im rik");
 		System.out.println("switch from older commit");
 	}
 	
 	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
 	
 	public static void main(String[] args) {
 		
 		Bp_Cnaby CnabyRik = new main().new Bp_Cnaby();
+		CnabyRik.setNome("Bobi");
+		System.out.println(CnabyRik.getNome());
+		
 		CnabyRik.saySomething().saySomething().saySomething().saySomething().saySomething();
 	}
 }
