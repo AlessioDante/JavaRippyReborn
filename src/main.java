b9a8a6d84e2176506a242af9beeb2f5031438566
Riccardo <riccardo.toni.85@gmail.com>
2018-02-15 14:53:09 +0100

    sorpresa! avvia il codice e ammira il risultato.

--- src/main.java
+++ src/main.java
@@ -1,7 +1,14 @@
 
 import java.util.Random;
 
-public class main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
+
+public class main extends Application{
 
 	///+-+-+-+-+-+-+-+-+-+-+-+-
 	// Ale SandBox 
@@ -13,7 +20,7 @@ public class main {
 				"Ps Mangio biscotti gtfo",
 				"grrr puzzi", 
 				"1,2,3 cacca in culo a te", 
-				"Polep Fresser", 
+				"Popel Fresser", 
 				"Dont mess with cnabo or get the stabbo", 
 				" hello there"};	
 	//+-+-+- Eventi 	
@@ -22,8 +29,8 @@ public class main {
 			System.out.println("cnaby dice : " + m_sloganList[rand.nextInt(5)]);
 			return this;
 		}		
-		public void setNome(String nnome) {
-			m_nome = nnome;
+		public void setNome(String nome) {
+			m_nome = nome;
 		}
 		public String getNome() {
 			return m_nome;
@@ -40,6 +47,22 @@ public class main {
 	public void rik_hello() {
 		System.out.println("im rik");
 		System.out.println("switch from older commit");
+		
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Group root = new Group();
+		Canvas canvas = new Canvas(700, 700);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		
+		Bear bear = new Bear();
+        bear.draw(gc);
+        
+        root.getChildren().add(canvas);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
 	}
 	
 	//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -51,5 +74,7 @@ public class main {
 		System.out.println(CnabyRik.getNome());
 		
 		CnabyRik.saySomething().saySomething().saySomething().saySomething().saySomething();
+		
+		launch(args);
 	}
 }
